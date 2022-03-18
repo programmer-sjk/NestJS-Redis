cf7b07bc49d0b22bfe75753015bbea94dd0adbcd
seojeongkuk <wjdrnr6662@gmail.com>
2022-03-18 12:12:41 +0900

    cachce manager를 통한 redis 테스트

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { CACHE_MANAGER, Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Cache } from 'cache-manager';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/test')
+  async getTest(): Promise<string> {
+    console.log(
+      await this.cacheManager.get('sess:3WZ5AsP415l30HIlGsS6kgGnGHgZCLS-'),
+      await this.cacheManager.get('abc'),
+    )
+    return 'testest';
+  }
+
+  @Get('/test2')
+  async getTest2(): Promise<string> {
+    console.log(
+      await this.cacheManager.set('abc', 'ddd'),
+    )
+    return 'testest';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.registerAsync({
+      useFactory: () => {
+        return {
+          store: redisStore,
+          host: 'localhost',
+          port: 6381,
+        }
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
