c78c7ce284d11c3cd6fdd5f4c39cc1e8eebcd1ce
seojeongkuk <wjdrnr6662@gmail.com>
2022-03-30 22:03:30 +0900

    Redis 강제 종료하는 closeNestApp 추가

---
+++ test/closeNestApp.ts
@@ -0,0 +1,8 @@
+import { CACHE_MANAGER } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common/interfaces/nest-application.interface';
+
+export async function closeNestApp(app: INestApplication) {
+  await app.close();
+  const redisClient: any = app.get(CACHE_MANAGER).store.getClient();
+  redisClient.quit()
+}
