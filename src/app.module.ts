c9c8ad23122906b8e18430ef57778f4edcad12f9
sjk5766 <wjdrnr6662@gmail.com>
2024-02-27 17:51:08 +0900

    feat: app.module 설정 수정 & 사용하지 app 관련 파일 제거

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RedisModule } from './redis/redis.module';
+import { PersonModule } from './person/person.module';
 
 @Module({
-  imports: [],
+  imports: [RedisModule, PersonModule],
   controllers: [AppController],
   providers: [AppService],
 })
