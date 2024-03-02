ee92098a8375a1c453a816cf7beaea1739cba201
sjk5766 <wjdrnr6662@gmail.com>
2024-03-02 15:31:07 +0900

    test: move service에 대한 테스트 코드 추가

--- src/movie/movie.service.spec.ts
+++ src/movie/movie.service.spec.ts
@@ -5,9 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from '../../ormconfig';
 import { MovieRepository } from './movie.repository';
 import { MovieModule } from './movie.module';
+import { RedisService } from '../redis/redis.service';
+import { RedisModule } from '../redis/redis.module';
 
 describe('MovieService', () => {
   let service: MovieService;
+  let redisService: RedisService;
   let movieRepository: MovieRepository;
 
   beforeEach(async () => {
@@ -16,12 +19,14 @@ describe('MovieService', () => {
         TypeOrmModule.forRoot({ ...connectionOptions, logging: false }),
         TypeOrmModule.forFeature([Movie]),
         MovieModule,
+        RedisModule,
       ],
-      providers: [MovieService, MovieRepository],
+      providers: [MovieService, RedisService, MovieRepository],
     }).compile();
 
-    movieRepository = module.get<MovieRepository>(MovieRepository);
     service = module.get<MovieService>(MovieService);
+    redisService = module.get<RedisService>(RedisService);
+    movieRepository = module.get<MovieRepository>(MovieRepository);
   });
 
   it('should be defined', () => {
@@ -44,6 +49,52 @@ describe('MovieService', () => {
       service.increaseRecommendCount(1),
       service.increaseRecommendCount(1),
       service.increaseRecommendCount(1),
+    ]);
+
+    // then
+    const movie = await movieRepository.findOne(1);
+    expect(movie.recommendCount).toBe(10);
+  });
+
+  it('set nx 동시에 10개 요청', async () => {
+    // given
+    // DB에 추천 수가 0인 1번 영화를 수동으로 만들어 둠
+
+    // when
+    await Promise.all([
+      service.increaseRecommendCountBySetNx(1),
+      service.increaseRecommendCountBySetNx(1),
+      service.increaseRecommendCountBySetNx(1),
+      service.increaseRecommendCountBySetNx(1),
+      service.increaseRecommendCountBySetNx(1),
+      service.increaseRecommendCountBySetNx(1),
+      service.increaseRecommendCountBySetNx(1),
+      service.increaseRecommendCountBySetNx(1),
+      service.increaseRecommendCountBySetNx(1),
+      service.increaseRecommendCountBySetNx(1),
+    ]);
+
+    // then
+    const movie = await movieRepository.findOne(1);
+    expect(movie.recommendCount).toBe(10);
+  });
+
+  it('redlock 동시에 10개 요청', async () => {
+    // given
+    // DB에 추천 수가 0인 1번 영화를 수동으로 만들어 둠
+
+    // when
+    await Promise.all([
+      service.increaseRecommendCountByRedLock(1),
+      service.increaseRecommendCountByRedLock(1),
+      service.increaseRecommendCountByRedLock(1),
+      service.increaseRecommendCountByRedLock(1),
+      service.increaseRecommendCountByRedLock(1),
+      service.increaseRecommendCountByRedLock(1),
+      service.increaseRecommendCountByRedLock(1),
+      service.increaseRecommendCountByRedLock(1),
+      service.increaseRecommendCountByRedLock(1),
+      service.increaseRecommendCountByRedLock(1),
     ]);
 
     // then
