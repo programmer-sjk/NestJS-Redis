f693ea54b0b2afdb8e066ccfeb77ac195094724b
sjk5766 <wjdrnr6662@gmail.com>
2024-03-02 15:30:51 +0900

    feat: redis redlock 사용 메소드 추가

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,10 +1,16 @@
 import { InjectRedis } from '@nestjs-modules/ioredis';
 import { Injectable } from '@nestjs/common';
 import { Redis } from 'ioredis';
+import Redlock from 'redlock';
 
 @Injectable()
 export class RedisService {
-  constructor(@InjectRedis() private redis: Redis) {}
+  private readonly redlock: Redlock;
+  private readonly lockDuration = 10_000;
+
+  constructor(@InjectRedis() private redis: Redis) {
+    this.redlock = new Redlock([redis]);
+  }
 
   async get(key: string) {
     return this.redis.get(key);
@@ -12,6 +18,14 @@ export class RedisService {
 
   async set(key: string, value: string) {
     return this.redis.set(key, value);
+  }
+
+  async setNx(key: string, value: string) {
+    return this.redis.set(key, value, 'PX', 1000, 'NX');
+  }
+
+  async del(key: string) {
+    return this.redis.del(key);
   }
 
   async zadd(key: string, score: number, member: string) {
@@ -24,5 +38,9 @@ export class RedisService {
 
   async zrevrange(key: string, max: number) {
     return this.redis.zrevrange(key, 0, max);
+  }
+
+  async acquireLock(key: string) {
+    return this.redlock.acquire([`lock:${key}`], this.lockDuration);
   }
 }
